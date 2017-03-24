91c80ebc0870102b14a683630095539f717d6baf
RaonePatel <raonepatel007@gmail.com>
2017-03-24 08:10:47 +0530

    Mobile_No
    This commit have access to insert mobile number in Database

---
+++ Mobile_No.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+public partial class Mobile_No : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|MobileCharge.mdf;Integrated Security=True");
+        con.Open();
+
+        string query = "insert into User_Details(Mobile_No) values('" + TextBox1.Text + "');";
+        SqlCommand cmd = new SqlCommand(query,con);
+        cmd.ExecuteNonQuery();
+        con.Close();
+    }
+}
